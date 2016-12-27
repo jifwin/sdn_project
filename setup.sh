68be127563594a1d04eaf75bdae9383c4cf92945
jifwin <gpietrusza@gmail.com>
2016-12-27 21:19:24 +0000

    add setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python git tmux
+sudo apt-get install python-pip
+sudo pip install ryu
+sudo apt-get install mininet
+sudo apt-get install python-eventlet python-routes python-webob python-paramiko
+git clone git://github.com/osrg/ryu.git
+pip install pprint
+cd ryu
