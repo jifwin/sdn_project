84159badf870f8797461ca853c018a17ecded7d4
Jaglarz Piotr <pjaglarz@student.agh.edu.pl>
2017-01-07 12:58:08 +0100

    add .gitignore

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ sudo pip install ryu
 sudo apt-get install mininet
 sudo apt-get install python-eventlet python-routes python-webob python-paramiko
 git clone git://github.com/osrg/ryu.git
-pip install pprint
+sudo pip install pprint
 cd ryu
